64d92affb0ba12eaf1b031913d1f64cd42cfed91
bradley.thomas60@googlemail.com <bradley.thomas60@googlemail.com>
2022-05-12 17:58:31 +0100

    Can successfully add new items with params, and view them

--- call_db.py
+++ call_db.py
@@ -1,13 +1,14 @@
 from distutils.util import execute
 from sqlite3 import *
 
-class database:
+class Database:
     def __init__(self):
         conn = connect("stock_tracker.db")
         cur = conn.cursor()
         cur.execute(
                     "CREATE TABLE IF NOT EXISTS stock("
                     "title VARCHAR(50) UNIQUE NOT NULL,"
+                    "garment VARCHAR(50) NOT NULL,"
                     "colour VARCHAR(50) NOT NULL,"
                     "price_make FLOAT NOT NULL,"
                     "price_sell FLOAT NOT NULL,"
@@ -19,20 +20,20 @@ class database:
         
         print("class called")
         
-    def add_to_db(self, title, colour, price_make, price_sell):
-        profit = price_sell - price_make
+    def add_to_db(self, title, garment, colour, price_make, price_sell):
+        self.profit = price_sell - price_make
         
         conn = connect("stock_tracker.db")
         cur = conn.cursor()
         cur.execute(
-                    "INSERT INTO stock (title, colour, price_make, price_sell, profit)"
-                    "VALUES((?), (?), (?), (?), (?))", 
-                    (title, colour, price_make, price_sell, profit)
+                    "INSERT INTO stock (title, garment, colour, price_make, price_sell, profit)"
+                    "VALUES((?), (?), (?), (?), (?), (?))", 
+                    (title, garment, colour, price_make, price_sell, self.profit)
                     )
         conn.commit()
         conn.close()
         
-        print(f"New item {title} added")
+        print(f"New item: {title} added")
         
     def view_db(self):
         conn = connect("stock_tracker.db")
@@ -45,4 +46,4 @@ class database:
         conn.close()
         
         for data in values:
-            return data
+            print(data)
--- main.py
+++ main.py
@@ -1,5 +1,42 @@
-from call_db import database as db
-from pandas import DataFrame
+from regex import P
+from call_db import Database
+from pandas import DataFrame as df
+
+tshirt_cost_make = 4.75
+tshirt_cost_sell = 15.0
+
+# mayday_tshirt = [ "Mayday t-shirt", "tshirt", "white", tshirt_cost_make, tshirt_cost_sell ]
+# dia_de_los_muertos = [ "Dia De Los Muertos", "tshirt", "white", tshirt_cost_make, tshirt_cost_sell ]
+
+mayday_tshirt = {
+    "title" : "Mayday",
+    "garment" : "tshirt",
+    "colour" : "white",
+    "price to make" : tshirt_cost_make,
+    "price to sell" : tshirt_cost_sell
+}
 
 if __name__ == "__main__":
-    pass
+    db = Database()
+    
+    def add_new_stock_item():
+        title = input("Title: ")
+        garment = input("Garment: ")
+        colour = input("Colour: ")
+        price_to_make = float(input("Price to make: "))
+        price_to_sell = float(input("Price to sell: "))
+        
+        db.add_to_db(title, garment, colour, price_to_make, price_to_sell)
+        
+    def view_all_stock_items():
+        print(db.view_db())
+        
+    user_input = int(input("What you wanna' do?"))
+    
+    if user_input == 1:
+        add_new_stock_item()
+    elif user_input == 2:
+        view_all_stock_items()
+    
+    
+    

1c6457f795f112e7f36e736bf7607396edf504bb
Bradley Thomas <40279277+japemasterBrad@users.noreply.github.com>
2022-05-14 16:12:57 +0100

    Added a looping menu function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#from regex import P
-from call_db import Database
+from database import Database
 from pandas import *
+from os import system
 
 tshirt_cost_make = 4.75
 tshirt_cost_sell = 15.0
@@ -26,22 +26,31 @@ if __name__ == "__main__":
         db.add_to_db(title, garment, colour, price_to_make, price_to_sell)
         
     def view_all_stock_items():
-        data = db.view_db()
+        datas = db.view_db()
         
-        df = DataFrame(data)
-        df.index = data[0]
+        df = DataFrame(data = mayday_tshirt, index=RangeIndex)
         
         print(df)
-        
-    user_input = int(input("What you wanna' do?\n"))
     
     def remove_stock_item():
-        user_input = input("Title of the item you'd like to remove: ")
-        db.remove_from_db(user_input)
+        print(db.remove_from_db())
     
-    if user_input == 1:
-        add_new_stock_item()
-    elif user_input == 2:
-        view_all_stock_items()
-    elif user_input == 3:
-        remove_stock_item()
+    def menu():
+        menu = True
+
+        system("clear") 
+        user_input = input("What you wanna' do?\n")
+
+        while (menu):
+            if user_input == "1":
+                menu = False
+                add_new_stock_item()
+            elif user_input == "2":
+                menu = False
+                view_all_stock_items()
+            elif user_input == "3":
+                menu = False
+                remove_stock_item()
+            else:
+                continue
+    menu()

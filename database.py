a53de0fc4d9a5e0aff68aa538cc8eefd54e3891f
bradley.thomas60@googlemail.com <bradley.thomas60@googlemail.com>
2022-05-17 18:25:08 +0100

    renaming filename

---
+++ database.py
@@ -0,0 +1,65 @@
+from distutils.util import execute
+from sqlite3 import *
+
+class Database:
+    def __init__(self):
+        conn = connect("stock_tracker.db")
+        cur = conn.cursor()
+        cur.execute(
+                    "CREATE TABLE IF NOT EXISTS stock("
+                    "id SERIAL PRIMARY KEY,"
+                    "title VARCHAR(50) UNIQUE NOT NULL,"
+                    "garment VARCHAR(50) NOT NULL,"
+                    "colour VARCHAR(50) NOT NULL,"
+                    "price_make FLOAT NOT NULL,"
+                    "price_sell FLOAT NOT NULL,"
+                    "profit FLOAT NOT NULL"
+                    ")"
+                    )
+        conn.commit()
+        conn.close()
+        
+        print("class called")
+        
+    def add_to_db(self, title, garment, colour, price_make, price_sell):
+        self.profit = price_sell - price_make
+        
+        conn = connect("stock_tracker.db")
+        cur = conn.cursor()
+        cur.execute(
+                    "INSERT INTO stock (title, garment, colour, price_make, price_sell, profit)"
+                    "VALUES((?), (?), (?), (?), (?), (?))", 
+                    (title, garment, colour, price_make, price_sell, self.profit)
+                    )
+        conn.commit()
+        conn.close()
+        
+        print(f"New item: {title} added")
+        
+    def view_db(self):
+        conn = connect("stock_tracker.db")
+        cur = conn.cursor()
+        cur.execute(
+                    "SELECT * FROM stock"
+                    )
+        values = cur.fetchall()
+        conn.commit()
+        conn.close()
+        
+        for data in values:
+            return data
+        
+            
+    def remove_from_db(self, item_to_remove):
+        conn = connect("stock_tracker.db")
+        cur = conn.cursor()
+        cur.execute(
+                    "DELETE FROM stock,"
+                    "WHERE title = (?)",
+                    item_to_remove
+                    )
+        conn.commit()
+        conn.close()
+        
+        print("{item_to_remove} has been removed from stocklist")
+            
